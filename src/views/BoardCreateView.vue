f45c2878c759466d6ab2790b255c22fd9c50e18f
GoHeeSeok00 <weeds1590@gmail.com>
2022-11-04 16:55:49 +0900

    ADD : 게시글 form 밸리데이션 추가
    - vee-validate 라이브러리 사용
    composition api 방식 구현
    Form, Field 사용

--- src/views/BoardCreateView.vue
+++ src/views/BoardCreateView.vue
@@ -4,9 +4,26 @@ import { useMutation, useQuery } from '@vue/apollo-composable'
 import { watchEffect, computed } from '@vue/runtime-core';
 import gql from 'graphql-tag'
 import { useRouter } from 'vue-router';
+import { Form, Field } from 'vee-validate';
+import * as yup from 'yup';
 
+// 라우터 
 const router = useRouter();
 
+
+// 게시글 밸리데이션
+const boardSchema = yup.object().shape({
+  title: yup.string().required(),
+  content: yup.string().required(),
+  status: yup.string().required(),
+});
+
+// const { errors } = useForm({
+//   validationSchema: boardSchema,
+// });
+
+
+// 게시글 생성
 const CREATE_BOARD = gql`
   mutation createBoard ($input: NewBoard) {
     createBoard(input: $input) {
@@ -21,7 +38,8 @@ const status = ref('');
 
 const { onDone, error, mutate: createBoard } = useMutation(CREATE_BOARD);
 
-const submitForm = function () {
+const submitForm = function (values) {
+  // alert('S\n\n' + JSON.stringify(values, null, 4))
   console.log(title, content, status);
   createBoard({
     input: {
@@ -31,6 +49,12 @@ const submitForm = function () {
     }
   });
   
+  console.log(error);
+  
+  watchEffect(() => {
+    console.log(`onDone: ${onDone}`);
+  })
+
   onDone((res) => {
     const boardId = computed(() => res?.data?.createBoard.id ?? "")
     console.log(boardId.value);
@@ -38,31 +62,38 @@ const submitForm = function () {
       path: `/boards/${boardId.value}`
     });
   })
-
-  watchEffect(() => {
-    console.log(`onDone: ${onDone}`);
-  })
 }
-
 
 
 </script>
 
 <template>
-  <form v-on:submit="submitForm">
+  <Form v-on:submit="submitForm" :validation-schema="boardSchema" v-slot="{ errors }">
     <div>
       <label for="title">제목: </label>
-      <input id="title" type="text" v-model="title" v-focus>
+      <Field id="title" name="title" type="text" v-model="title" v-focus />
+      <br>
+      <span class="validate-error">{{ errors.title }}</span>
     </div>
     <div>
       <label for="content">내용: </label>
-      <input id="content" type="content" v-model="content">
+      <Field id="content" name="content" type="text" v-model="content" />
+      <br>
+      <span class="validate-error">{{ errors.content }}</span>
     </div>
     <div>
       <label for="status">상태: </label>
-      <input id="status" type="status" v-model="status">
+      <Field id="status" name="status" type="text" v-model="status" />
+      <br>
+      <span class="validate-error">{{ errors.status }}</span>
     </div>
     <br>
     <button type="submit">작성하기</button>
-  </form>
+  </Form>
 </template>
+
+<style scoped>
+.validate-error {
+  color: red;
+}
+</style>
